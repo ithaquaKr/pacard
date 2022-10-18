a12c2bc16b238a5944cc4d3791e619cb4ef463ae
ithaqua <hiepnguyentuan1002@gmail.com>
2022-10-18 20:59:43 +0700

    init frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,57 @@
+import "./app.scss";
+
+import Auth from "./pages/auth/Auth";
+import Home from "./pages/home/Home";
+import Account from "./pages/account/Account";
+import MydocumentList from "./pages/mydocumentList/MydocumentList";
+import Landingpage from "./pages/landingpage/Landingpage";
+import Library from "./pages/library/Library";
+import Aboutus from "./pages/aboutus/Aboutus";
+import Todo from "./pages/todo/Todo";
+
+import Sidebar from "./components/sidebar/Sidebar";
+
+
+import {
+  BrowserRouter,
+  Route,
+  Navigate,
+  Routes,
+} from "react-router-dom";
+
+import { useContext } from "react";
+import { AuthContext } from "./context/authContext/AuthContext";
+
+const App = () => {
+  const { user } = useContext(AuthContext);
+
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          {user ? <Route path="/" element={<Landingpage/>} />: 
+          // <Redirect to="/auth" />
+          <Route render={() => <Navigate to="/auth"/>} />
+          }
+        </Route>
+        <Route path="/auth">{!user ? <Route path="/auth" element={<Auth/>} /> : <Navigate to="/home"/>}</Route>
+        {user && (
+          <>
+          <Sidebar/>
+          <div className="main-container">
+            <Route path="/home" element={<Home/>} />
+            <Route path="/mydocuments" element={<MydocumentList />} />
+            <Route path="/library" element={<Library/>} />
+            <Route path="/todo" element={<Todo/>} />
+            <Route path="/account" element={<Account/>} />
+            <Route path="/aboutus" element={<Aboutus/>} />
+          </div>
+          </>
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ client/src/firebase.js
@@ -0,0 +1,31 @@
+// import firebase from 'firebase/compat/app';
+// import 'firebase/storage';  
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAEjN1kfzJUvl1p-eRAZ1JdJNn_DNYrwb8",
+//   authDomain: "ptit-learn-app.firebaseapp.com",
+//   projectId: "ptit-learn-app",
+//   storageBucket: "ptit-learn-app.appspot.com",
+//   messagingSenderId: "149937631687",
+//   appId: "1:149937631687:web:0731f3a14ec85b7d10e9e2",
+//   measurementId: "G-Y98P92YX8W"
+// };
+const firebaseConfig = {
+    apiKey: "AIzaSyDRc0yI742yo4G0OF9OVsRp4gU4bYi9rgA",
+    authDomain: "pacard-thuhuong-f9f39.firebaseapp.com",
+    projectId: "pacard-thuhuong-f9f39",
+    storageBucket: "pacard-thuhuong-f9f39.appspot.com",
+    messagingSenderId: "1020141653299",
+    appId: "1:1020141653299:web:9b7a188736fa0eb035c246",
+    measurementId: "G-GGXPD6BW5J"
+  };
+  
+// firebase.initializeApp(firebaseConfig);
+// const storage = firebase.storage();
+// export default storage;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Cloud Storage and get a reference to the service
+const storage = getStorage(app);
+export default storage;
